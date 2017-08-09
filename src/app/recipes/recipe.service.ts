ad1c3a7ac38d005f3faac83ed33292183ff1a0cd
Tier5 LLC <github@tier5.us>
2017-08-09 22:35:46 +0530

    Ingredients shown on recpe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,36 @@
 import {EventEmitter, Output} from '@angular/core';
 
 import { Recipe } from './recipe.modal';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
   @Output() selectedRecipeEmitter = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe( 'Recipe Name 1', 'Recipe Description 1', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg' ),
-    new Recipe( 'Recipe Name 2', 'Recipe Description 2', 'http://www.seriouseats.com/images/2016/07/20160711-eggplant-recipes-roundup-01.jpg' )
+    new Recipe(
+      'Ricotta Salad',
+      'Yum Yum!',
+      'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg',
+      [
+        new Ingredient('Bread', 1),
+        new Ingredient('Carrot', 1),
+        new Ingredient('Potato', 1),
+        new Ingredient('Meat', 1)
+      ]
+    ),
+    new Recipe(
+      'Eggplant',
+      'Hum Hum',
+      'http://www.seriouseats.com/images/2016/07/20160711-eggplant-recipes-roundup-01.jpg',
+      [
+        new Ingredient('Egg', 2),
+        new Ingredient('Onion', 1),
+        new Ingredient('Fish', 1),
+        new Ingredient('Meat', 1)
+      ]
+
+    )
   ];
 
   getRecipes() {
