e71dec57d649e645a8549478f8272d35ff50ffa3
Tier5 LLC <github@tier5.us>
2017-08-09 23:09:33 +0530

    Added ingredient to shoppping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.modal';
 import {RecipeService} from '../recipe.service';
+import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,9 +13,17 @@ import {RecipeService} from '../recipe.service';
 export class RecipeDetailComponent implements OnInit {
 
   @Input() recipeData: Recipe;
-  constructor( private recipeService: RecipeService) { }
+  constructor( private recipeService: RecipeService, private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+
+    for (const value of ingredients) {
+      const newIng = new Ingredient(value.name, value.amount);
+      this.shoppingListService.addIngredient(newIng);
+    }
   }
 
 }
