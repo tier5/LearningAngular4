2fbc6635d501153058cf6ef6e30281e2140f07c3
Tier5 LLC <github@tier5.us>
2017-08-09 15:38:56 +0530

    Understanding @ViewChild()

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild( 'newRecipeName' ) newRecipeNameRef: ElementRef;
+  @ViewChild( 'newRecipeAmount' ) newRecipeAmountRef: ElementRef;
+
+  @Output() newIngredientAddedEmitter = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+
+    const ingName = this.newRecipeNameRef.nativeElement.value;
+    const ingAmount = this.newRecipeAmountRef.nativeElement.value;
+    const ingredientAdded = new Ingredient( ingName, ingAmount);
+    this.newIngredientAddedEmitter.emit(ingredientAdded);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -15,6 +16,10 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onNewIngredientAdded(newIngredient: Ingredient) {
+    this.ingredients.push(newIngredient);
   }
 
 }
