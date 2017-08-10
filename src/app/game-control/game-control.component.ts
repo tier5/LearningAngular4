9b92ba6798f64fd0bbdd2205b81f1ac3b63967a2
Tier5 LLC <github@tier5.us>
2017-08-10 22:20:59 +0530

    Custom Observable Created

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,17 +1,20 @@
-import {Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
   styleUrls: ['./game-control.component.css']
 })
-export class GameControlComponent implements OnInit {
+export class GameControlComponent implements OnInit, OnDestroy {
 
   @Output() gameStartedEvent = new EventEmitter<number>();
 
   timer: any;
   counter: number = 0;
-
+  customObservable: Subscription;
   constructor() { }
 
   startGame() {
@@ -25,8 +28,36 @@ export class GameControlComponent implements OnInit {
     clearInterval(this.timer);
   }
 
-
   ngOnInit() {
+
+    const myObservable = Observable.create( ( observer: Observer<string>) => {
+      setTimeout( () => {
+       observer.next('1st Data Packet!');
+      }, 2000);
+      setTimeout( () => {
+        observer.next('2nd Data Packet!');
+      }, 4000);
+      // setTimeout( () => {
+      //   observer.error('Error Occured!');
+      // }, 5000);
+      setTimeout( () => {
+        observer.complete();
+      }, 5000);
+      setTimeout( () => {
+        observer.next('3rd Data Packet!');
+      }, 6000);
+
+    });
+
+    this.customObservable = myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('Completed'); }
+    );
+  }
+
+  ngOnDestroy() {
+    this.customObservable.unsubscribe();
   }
 
 }
